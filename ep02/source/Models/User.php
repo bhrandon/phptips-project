c3ce4111e11a960aef77950bdaa90e62fc49535b
Bhrandon B <bhrandonbmt@gmail.com>
2019-10-04 08:08:43 -0300

    Abstração do banoc em uma camada de modelo

---
+++ ep02/source/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Source\Models;
+
+use CoffeeCode\DataLayer\DataLayer;
+
+class User extends DataLayer
+{
+    public function __construct()
+    {   
+        //string $entity, array $required, string $primary = 'id', bool $timestamps = true
+        parent::__construct("users", ["first_name", "last_name"], "idusers");
+    }
+
+    
+    public function addresses()
+    {
+        return (new Address())->find("id_users = :uid", "uid={$this->idusers}")->fetch(true);   
+    }
+}
